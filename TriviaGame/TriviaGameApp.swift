9c2cacaac892e71d40e445e12df82093ef57e672
courtneymahugu <courtneymahugu@gmail.com>
2025-04-01 15:40:09 -0400

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Courtney Mahugu on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
